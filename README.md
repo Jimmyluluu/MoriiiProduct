9a8a9809959cf405c5faa30181da107317189f71
Minnn <mandychung2011@hotmail.com>
2023-07-20 23:08:46 +0800

    新增三大商品之card

--- README.md
+++ README.md
@@ -1,19 +1,28 @@
 # 商品小網頁
+
 ## 使用環境
+
 - Node v18.16.0
 - Npm v9.5.1
 - Vue3 + Vite
 - Tailwind CSS
+
 ## 如何開始
+
 安裝需要套件
+
 ```bash
 npm install
 ```
+
 跑起來
+
 ```bash
 npm run dev
 ```
+
 養成好習慣要縮排
+
 ```bash
 npm run prettier
 ```
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -9,16 +9,65 @@ const count = ref(0);
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <img
-      src="https://github.com/Jimmyluluu/image/blob/master/Standingsign.png?raw=true"
-    />
-    <img
-      src="https://github.com/Jimmyluluu/image/blob/master/Card.png?raw=true"
-    />
+  <h1>歡迎光臨 Moriii's Store</h1>
+  <div class="columns-3">
+    <div class="card w-96 bg-base-100 shadow-xl">
+      <figure>
+        <img
+          src="https://github.com/Jimmyluluu/image/blob/master/Standingsign.png?raw=true"
+          alt="Shoes"
+        />
+      </figure>
+      <div class="card-body">
+        <h2 class="card-title">
+          Shoes!
+          <div class="badge badge-secondary">NEW</div>
+        </h2>
+        <p>If a dog chews shoes whose shoes does he choose?</p>
+        <div class="card-actions justify-end">
+          <div class="badge badge-outline">Fashion</div>
+          <div class="badge badge-outline">Products</div>
+        </div>
+      </div>
+    </div>
+    <div class="card w-96 bg-base-100 shadow-xl">
+      <figure>
+        <img
+          src="https://github.com/Jimmyluluu/image/blob/master/Standingsign.png?raw=true"
+          alt="Shoes"
+        />
+      </figure>
+      <div class="card-body">
+        <h2 class="card-title">
+          Shoes!
+          <div class="badge badge-secondary">NEW</div>
+        </h2>
+        <p>If a dog chews shoes whose shoes does he choose?</p>
+        <div class="card-actions justify-end">
+          <div class="badge badge-outline">Fashion</div>
+          <div class="badge badge-outline">Products</div>
+        </div>
+      </div>
+    </div>
+    <div class="card w-96 bg-base-100 shadow-xl">
+      <figure>
+        <img
+          src="https://github.com/Jimmyluluu/image/blob/master/Standingsign.png?raw=true"
+          alt="Shoes"
+        />
+      </figure>
+      <div class="card-body">
+        <h2 class="card-title">
+          Shoes!
+          <div class="badge badge-secondary">NEW</div>
+        </h2>
+        <p>If a dog chews shoes whose shoes does he choose?</p>
+        <div class="card-actions justify-end">
+          <div class="badge badge-outline">Fashion</div>
+          <div class="badge badge-outline">Products</div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
