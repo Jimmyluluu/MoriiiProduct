3d533941c8c5de420709278ea3a1c9ebdeae266d
Jimmyluluu <luu21@kimo.com>
2023-07-21 01:28:47 +0800

    add flex

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,7 @@ import HelloWorld from "./components/HelloWorld.vue";
 </script>
 
 <template>
-  <HelloWorld/>
+  <HelloWorld />
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -10,8 +10,8 @@ const count = ref(0);
 
 <template>
   <h1>歡迎光臨 Moriii's Store</h1>
-  <div class="columns-3">
-    <div class="card w-96 bg-violet-300 et-300 shadow-xl">
+  <div class="flex flex-row flex-nowrap md:flex-10">
+    <div class="card basis-1/3 bg-violet-300 et-300 shadow-xl">
       <figure>
         <img
           src="https://github.com/Jimmyluluu/image/blob/master/Selengangar-2.png?raw=true"
@@ -21,7 +21,11 @@ const count = ref(0);
       <div class="card-body">
         <h2 class="card-title text-white font-black">
           Dragoons 盲抽吊飾
-          <div class="inline-flex items-center gap-1.5 py-1.5 px-3 rounded-full text-xs font-medium bg-pink-400 text-white">NEW</div>
+          <div
+            class="inline-flex items-center gap-1.5 py-1.5 px-3 rounded-full text-xs font-medium bg-pink-400 text-white"
+          >
+            NEW
+          </div>
         </h2>
         <p class="text-white">6*6 cm / 1抽 NT.100 / 3抽優惠價 NT.250</p>
         <div class="card-actions justify-end">
@@ -30,7 +34,7 @@ const count = ref(0);
         </div>
       </div>
     </div>
-    <div class="card w-96 bg-violet-300 shadow-xl">
+    <div class="card basis-1/3 bg-violet-300 shadow-xl">
       <figure>
         <img
           src="https://github.com/Jimmyluluu/image/blob/master/Luxiem.png?raw=true"
@@ -40,16 +44,22 @@ const count = ref(0);
       <div class="card-body">
         <h2 class="card-title text-white font-black">
           Luxiem 飯友
-          <div class="inline-flex items-center gap-1.5 py-1.5 px-3 rounded-full text-xs font-medium bg-pink-400 text-white">NEW</div>
+          <div
+            class="inline-flex items-center gap-1.5 py-1.5 px-3 rounded-full text-xs font-medium bg-pink-400 text-white"
+          >
+            NEW
+          </div>
         </h2>
-        <p class="text-white">4*4 cm / 1隻 NT.110 / Luxiem包團優惠價 NT.500 加贈一張限定款明信片</p>
+        <p class="text-white">
+          4*4 cm / 1隻 NT.110 / Luxiem包團優惠價 NT.500 加贈一張限定款明信片
+        </p>
         <div class="card-actions justify-end">
           <div class="badge badge-outline text-violet-500">Fashion</div>
           <div class="badge badge-outline text-violet-500">Products</div>
         </div>
       </div>
     </div>
-    <div class="card w-96 bg-violet-300 shadow-xl">
+    <div class="card basis-1/3 bg-violet-300 shadow-xl">
       <figure>
         <img
           src="https://github.com/Jimmyluluu/image/blob/master/%E5%AE%A3%E5%82%B3%E8%BB%8A_selen%E8%80%BF%E9%AC%BC.png?raw=true"
@@ -59,7 +69,11 @@ const count = ref(0);
       <div class="card-body">
         <h2 class="card-title text-white font-black">
           小立牌
-          <div class="inline-flex items-center gap-1.5 py-1.5 px-3 rounded-full text-xs font-medium bg-pink-400 text-white">NEW</div>
+          <div
+            class="inline-flex items-center gap-1.5 py-1.5 px-3 rounded-full text-xs font-medium bg-pink-400 text-white"
+          >
+            NEW
+          </div>
         </h2>
         <p class="text-white">高 9~10 cm / 1隻 NT.150</p>
         <div class="card-actions justify-end">
