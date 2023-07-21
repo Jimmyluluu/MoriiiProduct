0e99315958df5d990f44a0fd6fa9a9f9bd4cd4fb
Jimmyluluu <luu21@kimo.com>
2023-07-21 21:43:04 +0800

    修好了

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,15 +2,42 @@
 import { ref } from "vue";
 const items = ref([
   {
-    imgUrl: "https://github.com/Jimmyluluu/image/blob/master/Selengangar-2.png?raw=true",
+    imgUrl:
+      "https://github.com/Jimmyluluu/image/blob/master/Selengangar-2.png?raw=true",
     title: "Dragoons 盲抽吊飾",
-    specification: "6*6 cm / 1抽 NT.100 / 3抽優惠價 NT.250"
+    specification: "6*6 cm / 1抽 NT.100 / 3抽優惠價 NT.250",
   },
   {
-    imgUrl: "https://github.com/Jimmyluluu/image/blob/master/Luxiem.png?raw=true",
+    imgUrl:
+      "https://github.com/Jimmyluluu/image/blob/master/Luxiem.png?raw=true",
     title: "Luxiem 飯友",
-    specification:"4*4 cm / 1隻 NT.110 / Luxiem包團優惠價 NT.500 加贈一張限定款明信片"
-  }
+    specification:
+      "4*4 cm / 1隻 NT.110 / Luxiem包團優惠價 NT.500 加贈一張限定款明信片",
+  },
+  {
+    imgUrl:
+      "https://raw.githubusercontent.com/Jimmyluluu/image/master/%E5%AE%A3%E5%82%B3%E8%BB%8A_selen%E8%80%BF%E9%AC%BC.png",
+    title: "小立牌",
+    specification: "高 9~10 cm / 1隻 NT.150",
+  },
+  {
+    imgUrl:
+      "https://github.com/Jimmyluluu/image/blob/master/Selengangar.png?raw=true",
+    title: "Selen & 耿鬼飯友",
+    specification: "4*4 cm / 1隻 NT.110",
+  },
+  {
+    imgUrl:
+      "https://github.com/Jimmyluluu/image/blob/master/Selengangar-3.png?raw=true",
+    title: "明信片",
+    specification: "A6 / 1張 NT.30",
+  },
+  {
+    imgUrl:
+      "https://github.com/Jimmyluluu/image/blob/master/Picture.png?raw=true",
+    title: "拍立得 feat.可卿",
+    specification: "1張 NT.70 (寫字or畫圖 NT.90)",
+  },
 ]);
 </script>
 
@@ -39,129 +66,30 @@ const items = ref([
     </div>
   </div>
   <!-- <h1>歡迎光臨 Moriii's Store</h1> -->
-  <div class="flex flex-col gap-8">
-    <div class="flex flex-row flex-nowrap gap-8 md:flex-10 w-5/6">
-      <div v-for="item in items" class="card basis-1/3 bg-violet-300 et-300 shadow-xl">
-        <figure>
-          <img
-            :src="item.imgUrl"
-            alt="Shoes"
-          />
-        </figure>
-        <div class="card-body">
-          <h2 class="card-title text-white font-black">
-            {{item.title}}
-            <div
-              class="inline-flex items-center gap-1.5 py-1.5 px-3 rounded-full text-xs font-medium bg-pink-400 text-white"
-            >
-              NEW
-            </div>
-          </h2>
-          <p class="text-white">{{item.specification}}</p>
-          <div class="card-actions justify-end">
-            <div class="badge badge-outline text-violet-500">Charm</div>
-            <div class="badge badge-outline text-violet-500">Products</div>
+  <div class="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
+    <div v-for="item in items" class="card bg-violet-300 et-300 shadow-xl">
+      <figure>
+        <img :src="item.imgUrl" alt="Shoes" />
+      </figure>
+      <div class="card-body">
+        <h2 class="card-title text-white font-black">
+          {{ item.title }}
+          <div
+            class="inline-flex items-center gap-1.5 py-1.5 px-3 rounded-full text-xs font-medium bg-pink-400 text-white"
+          >
+            NEW
           </div>
-        </div>
-      </div>
-    
-      <div class="card basis-1/3 bg-violet-300 shadow-xl">
-        <figure>
-          <img
-            src="https://github.com/Jimmyluluu/image/blob/master/%E5%AE%A3%E5%82%B3%E8%BB%8A_selen%E8%80%BF%E9%AC%BC.png?raw=true"
-            alt="Shoes"
-          />
-        </figure>
-        <div class="card-body">
-          <h2 class="card-title text-white font-black">
-            小立牌
-            <div
-              class="inline-flex items-center gap-1.5 py-1.5 px-3 rounded-full text-xs font-medium bg-pink-400 text-white"
-            >
-              NEW
-            </div>
-          </h2>
-          <p class="text-white">高 9~10 cm / 1隻 NT.150</p>
-          <div class="card-actions justify-end">
-            <div class="badge badge-outline text-violet-500">Acryl Stand</div>
-            <div class="badge badge-outline text-violet-500">Products</div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="flex flex-row flex-nowrap gap-8 md:flex-10 w-5/6">
-      <div class="card basis-1/3 bg-violet-300 et-300 shadow-xl">
-        <figure>
-          <img
-            src="https://github.com/Jimmyluluu/image/blob/master/Selengangar.png?raw=true"
-            alt="Shoes"
-          />
-        </figure>
-        <div class="card-body">
-          <h2 class="card-title text-white font-black">
-            Selen & 耿鬼飯友
-            <div
-              class="inline-flex items-center gap-1.5 py-1.5 px-3 rounded-full text-xs font-medium bg-pink-400 text-white"
-            >
-              NEW
-            </div>
-          </h2>
-          <p class="text-white">4*4 cm / 1隻 NT.110</p>
-          <div class="card-actions justify-end">
-            <div class="badge badge-outline text-violet-500">Acryl Stand</div>
-            <div class="badge badge-outline text-violet-500">Products</div>
-          </div>
-        </div>
-      </div>
-      <div class="card basis-1/3 bg-violet-300 shadow-xl">
-        <figure>
-          <img
-            src="https://github.com/Jimmyluluu/image/blob/master/Selengangar-3.png?raw=true"
-            alt="Shoes"
-          />
-        </figure>
-        <div class="card-body">
-          <h2 class="card-title text-white font-black">
-            明信片
-            <div
-              class="inline-flex items-center gap-1.5 py-1.5 px-3 rounded-full text-xs font-medium bg-pink-400 text-white"
-            >
-              NEW
-            </div>
-          </h2>
-          <p class="text-white">A6 / 1張 NT.30</p>
-          <div class="card-actions justify-end">
-            <div class="badge badge-outline text-violet-500">Acryl Stand</div>
-            <div class="badge badge-outline text-violet-500">Products</div>
-          </div>
-        </div>
-      </div>
-      <div class="card basis-1/3 bg-violet-300 shadow-xl">
-        <figure>
-          <img
-            src="https://github.com/Jimmyluluu/image/blob/master/Picture.png?raw=true"
-            alt="Shoes"
-          />
-        </figure>
-        <div class="card-body">
-          <h2 class="card-title text-white font-black">
-            拍立得 feat.可卿
-            <div
-              class="inline-flex items-center gap-1.5 py-1.5 px-3 rounded-full text-xs font-medium bg-pink-400 text-white"
-            >
-              NEW
-            </div>
-          </h2>
-          <p class="text-white">1張 NT.70 (寫字or畫圖 NT.90)</p>
-          <div class="card-actions justify-end">
-            <div class="badge badge-outline text-violet-500">Polaroid</div>
-            <div class="badge badge-outline text-violet-500">Products</div>
-          </div>
+        </h2>
+        <p class="text-white">{{ item.specification }}</p>
+        <div class="card-actions justify-end">
+          <div class="badge badge-outline text-violet-500">Charm</div>
+          <div class="badge badge-outline text-violet-500">Products</div>
         </div>
       </div>
     </div>
   </div>
-
+  <br>
+  <br>
   <footer
     class="footer w-full p-10 text-violet-50"
     style="background-color: #8b75ba"
