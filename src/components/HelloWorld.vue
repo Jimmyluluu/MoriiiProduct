a6bdb0a34f5597b95bf99e1ebb8ec67b333ec1fa
minnn311 <d0845235@0365.fcu.edu.tw>
2023-07-21 15:55:32 +0800

    new header

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -9,6 +9,27 @@ const count = ref(0);
 </script>
 
 <template>
+  <div class="navbar bg-white">
+  <div class="flex-1">
+    <a class="btn btn-ghost normal-case text-xl">daisyUI</a>
+  </div>
+  <div class="flex-none">
+    <ul class="menu menu-horizontal px-1">
+      <li><a>賣場連結</a></li>
+      <li>
+        <details>
+          <summary>
+            聯繫我
+          </summary>
+          <ul class="p-2 bg-base-100">
+            <li><a>Facebook</a></li>
+            <li><a>Mail</a></li>
+          </ul>
+        </details>
+      </li>
+    </ul>
+  </div>
+</div>
   <h1>歡迎光臨 Moriii's Store</h1>
   <div class="flex flex-row flex-nowrap md:flex-10">
     <div class="card basis-1/3 bg-violet-300 et-300 shadow-xl">
@@ -29,7 +50,7 @@ const count = ref(0);
         </h2>
         <p class="text-white">6*6 cm / 1抽 NT.100 / 3抽優惠價 NT.250</p>
         <div class="card-actions justify-end">
-          <div class="badge badge-outline text-violet-500">Fashion</div>
+          <div class="badge badge-outline text-violet-500">Charm</div>
           <div class="badge badge-outline text-violet-500">Products</div>
         </div>
       </div>
@@ -54,7 +75,7 @@ const count = ref(0);
           4*4 cm / 1隻 NT.110 / Luxiem包團優惠價 NT.500 加贈一張限定款明信片
         </p>
         <div class="card-actions justify-end">
-          <div class="badge badge-outline text-violet-500">Fashion</div>
+          <div class="badge badge-outline text-violet-500">Acryl Stand</div>
           <div class="badge badge-outline text-violet-500">Products</div>
         </div>
       </div>
@@ -77,7 +98,7 @@ const count = ref(0);
         </h2>
         <p class="text-white">高 9~10 cm / 1隻 NT.150</p>
         <div class="card-actions justify-end">
-          <div class="badge badge-outline text-violet-500">Fashion</div>
+          <div class="badge badge-outline text-violet-500">Acryl Stand</div>
           <div class="badge badge-outline text-violet-500">Products</div>
         </div>
       </div>
